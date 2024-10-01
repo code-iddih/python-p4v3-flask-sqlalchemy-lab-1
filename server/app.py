79fda90252510c9456305b84bb85f4bdc8f8e754
code-iddih <idinyamwaya@gmail.com>
2024-10-01 13:00:09 +0300

    Def Model, Init db, Adds views to get earthhquake by id and minimuum magnitude value

--- server/app.py
+++ server/app.py
@@ -1,7 +1,7 @@
 # server/app.py
 #!/usr/bin/env python3
 
-from flask import Flask, make_response
+from flask import Flask, make_response, jsonify
 from flask_migrate import Migrate
 
 from models import db, Earthquake
@@ -21,6 +21,41 @@ def index():
     return make_response(body, 200)
 
 # Add views here
+
+@app.route('/earthquakes/<int:id>', methods=['GET'])
+def get_earthquake(id):
+    earthquake = Earthquake.query.get(id)
+    if earthquake is None:
+        response = {"message": f"Earthquake {id} not found."}
+        return make_response(jsonify(response), 404)
+    
+    response = {
+        "id": earthquake.id,
+        "location": earthquake.location,
+        "magnitude": earthquake.magnitude,
+        "year": earthquake.year
+    }
+    return make_response(jsonify(response), 200)
+
+@app.route('/earthquakes/magnitude/<float:magnitude>')
+def get_earthquakes_by_magnitude(magnitude):
+    quakes = Earthquake.query.filter(Earthquake.magnitude >= magnitude).all()
+    
+    quake_data = []
+    for quake in quakes:
+        quake_data.append({
+            'id': quake.id,
+            'location': quake.location,
+            'magnitude': quake.magnitude,
+            'year': quake.year
+        })
+    
+    response_body = {
+        'count': len(quake_data),
+        'quakes': quake_data
+    }
+    
+    return jsonify(response_body), 200
 
 
 if __name__ == '__main__':
